c09afb278277eece47af5ccfbf73735daa9cb1c7
Chetna Gadam <chetnagadam@gmail.com>
2025-04-04 15:23:26 -0500

    Initial commit with comments

---
+++ NotesGenerator.py
@@ -0,0 +1,19 @@
+# Import Libraries
+
+# Initialize app
+
+# Load model for speech-to-text
+
+# Load Summarization model
+
+# Transcribe audio
+
+# summarize text
+
+# generate questions
+
+# process input audio
+
+# process input text
+
+# build UI
